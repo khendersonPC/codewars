691b03c154ae7a35e1e7cf2c177d7c85f2beae42
khendersonPC <20666083+khendersonPC@users.noreply.github.com>
2021-05-31 20:30:05 -0600

    whoops- forgot 0 condition

--- xbonacci.java
+++ xbonacci.java
@@ -1,22 +1,28 @@
 public class Xbonacci {
 
-  public double[] tribonacci(double[] s, int n) {
-      double[] results= new double[n];
-    
-      results[0] = s[0];
-      results[1] = s[1];
-      results[2] = s[2];
-    
-    for (int i = 3; i < results.length; i++){
-      results[i]= results[i-1]+ results[i-2]+ results[i-3];
+    public double[] tribonacci(double[] s, int n) {
+        double[] results= new double[n];
+      
+      if(n==0){
+         double[] x = {};
+         return x;
+       }
+      
+        results[0] = s[0];
+      if(n==1){
+        return results;
+      }
+        results[1] = s[1];
+      if(n==2){
+        return results;
+      }
+        results[2] = s[2];
+      
+      for (int i = 3; i < results.length; i++){
+        results[i]= results[i-1]+ results[i-2]+ results[i-3];
+      }
+       
+        return results;
+       
     }
-     if(n==0){
-       double[] x = {};
-       return x;
-     }
-    else{
-      return results;
-    }
-        
   }
-}
